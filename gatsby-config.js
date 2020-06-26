6e6ffcacd171ccc332ad09e6fa536aaf4de072f4
Richard Dimalanta <chardmd.au@gmail.com>
2020-06-27 08:19:23 +1000

    added gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
-const path = require(`path`);
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    title: `ImageGallery`,
+    title: "ImageGallery",
   },
   plugins: [
     /*
@@ -19,55 +19,56 @@ module.exports = {
      * tree.
      */
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `data`,
-        path: path.join(__dirname, `data`),
+        name: "data",
+        path: path.join(__dirname, "data"),
       },
     },
     // This plugin exposes helper functions for processing
     // images with the NPM package “sharp”. It's used by
     // several other plugins.
-    `gatsby-plugin-sharp`,
+    "gatsby-plugin-sharp",
     // This plugin identifies file nodes that are images and
     // transforms these to create new “ImageSharp” nodes.
     // With them you can resize images and
     // generate responsive image thumbnails.
-    `gatsby-transformer-sharp`,
+    "gatsby-transformer-sharp",
     // This plugin transforms JSON file nodes.
-    `gatsby-transformer-json`,
+    "gatsby-transformer-json",
     // This plugin takes your configuration and generates a
     // web manifest file so ImageGallery can be added to your
     // homescreen on Android.
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `ImageGallery`,
-        short_name: `ImageGallery`,
-        start_url: `/`,
-        background_color: `#f7f7f7`,
-        theme_color: `#191919`,
-        display: `minimal-ui`,
+        name: "ImageGallery",
+        short_name: "ImageGallery",
+        start_url: "/",
+        background_color: "#f7f7f7",
+        theme_color: "#191919",
+        display: "minimal-ui",
       },
     },
-    `gatsby-plugin-sass`,
+    "gatsby-plugin-sass",
     // This plugin generates a service worker and AppShell
     // html file so the site works offline and is otherwise
     // resistant to bad networks. Works with almost any
     // site!
-    `gatsby-plugin-offline`,
+    "gatsby-plugin-offline",
     // This plugin sets up Google Analytics for you.
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: `UA-168810586-1`, //tmp
+        trackingId: "UA-168810586-1", // tmp
       },
     },
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        pathToConfigModule: "src/utils/typography",
       },
     },
+    "gatsby-plugin-netlify",
   ],
 };
